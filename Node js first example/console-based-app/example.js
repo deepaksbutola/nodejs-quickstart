f35554fb8a97438c6e1d43e89b148a9f620abf11
Deepak Singh Butola <deepaksbutola098@gmail.com>
2017-05-01 21:14:49 +0530

    Node js console based and web based applications

---
+++ Node js first example/console-based-app/example.js
@@ -0,0 +1,10 @@
+//Node js log module
+// Simple log
+console.log('Hello world');
+//Log with format specifier
+console.log('Hello %s', 'World');   
+//Warn log : The console.warn() function is used to display warning message on console.
+const name = 'John';  
+console.warn(`Don't mess with me ${name}! Don't mess with me!`);
+// Error log : The console.error() function is used to render error message on console.
+console.error(new Error('Hell! This is a wrong method.'));  
